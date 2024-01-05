9144255bb07a8b5c9e8a9dfd21fa62e3361e05b9
Muthulingam Rajendran <muthulingam1410@gmail.com>
2024-01-05 14:07:40 +0530

    second commit

--- src/Components/MyProfile/index.js
+++ src/Components/MyProfile/index.js
@@ -10,7 +10,7 @@ const MyProfile = () => (
                 <p>Contacts</p>
             </div>
         </div>
-        <p className="role">UI/UX Designer</p>
+        <p className="role">Frontend Engineer</p>
         <div className="my-profile-content-container">
             <h1 className="my-profile-heading">Hello,  my name is Muthulingam</h1>
             <p className="my-profile-description">As a Full-stack Developer based in Chennai, I bring expertise in HTML, CSS, Python, and JavaScript, with proficiency in React.js, Node.js, Express, and SQLite. Developed projects include an E-commerce platform (Nxt Trendz), a YouTube-inspired app (Nxt Watch), Todos Application, and Wikipedia Search Application. Certified in React JS, Node JS, SQL, Python, and JavaScript Essentials. Connect with me on GitHub and LinkedIn.</p>
